a79f748a74c869d78e3be59b4d8f829caf9838b0
wp-newbiefriendly <locokoks@gmail.com>
2026-03-16 14:37:57 +0100

    vezba #1 - odradjena vezba

--- Domaci-1/script-vezba.js
+++ Domaci-1/script-vezba.js
@@ -1,3 +1,5 @@
+// Osnovne JavaScript vežbe – VARIJABLE
+
 let ime = "Ana";
 console.log(ime);
 
@@ -11,3 +13,103 @@ let grad = "Beograd";
 console.log("Zivim u gradu " + grad + ".");
 
 console.log("Zovem se " + ime);
+
+
+
+// Brojevi i math operatori
+
+let a = 10;
+let b = 5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+a = a + 1;
+console.log(a);
+
+b = b - 1;
+console.log(b);
+
+let x = 3;
+let y = 4;
+console.log((x + y) * 2);
+
+
+
+// String vežbe + replace()
+
+console.log("Ucim programiranje");
+console.log("Ucim programiranje".replace("programiranje", "kodiranje"));
+
+console.log("Danas je lep dan");
+console.log("Danas je lep dan".replace("lep", "suncan"));
+
+console.log("Moje ime je Ana");
+console.log("Moje ime je Ana".replace("Ana", "Marko"));
+
+// Kombinovane vežbe
+
+let poruka = "Ucim osnove Javascript";
+console.log(poruka.replace("osnove", "napredne"));
+
+let broj = 10;
+console.log("Vrednost broja je " + broj);
+
+console.log("Imam " + godine + " godina. ");
+
+let jeOnline = false;
+console.log(jeOnline);
+
+let c = 7;
+let d = 2;
+console.log(c+d);
+
+console.log("Javascript je zabavan");
+console.log("Javascript je zabavan".replace("zabavan", "koristan"));
+
+let rezultat = c * d;
+console.log(rezultat);
+
+console.log("Rezultat je " + rezultat);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let ime = "Ana";
+// console.log(ime);
+//
+// let godine = 20;
+// console.log(godine);
+//
+// let jeStudent = true;
+// console.log(jeStudent);
+//
+// let grad = "Beograd";
+// console.log("Zivim u gradu " + grad + ".");
+//
+// console.log("Zovem se " + ime);
+//
+// let tekst = "Ucim programiranje";
+//
+// let noviTekst = tekst.replace("programiranje", "kodiranje");
+//
+// console.log(noviTekst);
