3ffcdf4ef96d612f612be26e5adba5ec5aefa0ca
wp-newbiefriendly <locokoks@gmail.com>
2026-03-16 01:42:58 +0100

    Initial commit

---
+++ Domaci-1/script.js
@@ -0,0 +1,18 @@
+let oprema = ["racunar", "tastatura", "mis"];
+
+oprema.push("monitor");
+
+console.log(oprema);
+
+console.log("Prva stavka:", oprema[0]);
+console.log("Druga stavka:", oprema[1]);
+console.log("Treca stavka:", oprema[2]);
+
+
+//      [] → array
+//
+//     , → razdvaja elemente
+//
+//     [0] [1] [2] → pristup elementima
+//
+//     .push() → dodavanje
