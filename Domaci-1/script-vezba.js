10de0f7dd6239b8188c8f8076a8088490554a980
wp-newbiefriendly <locokoks@gmail.com>
2026-03-16 05:36:52 +0100

    vezba #1

---
+++ Domaci-1/script-vezba.js
@@ -0,0 +1,13 @@
+let ime = "Ana";
+console.log(ime);
+
+let godine = 20;
+console.log(godine);
+
+let jeStudent = true;
+console.log(jeStudent);
+
+let grad = "Beograd";
+console.log("Zivim u gradu " + grad + ".");
+
+console.log("Zovem se " + ime);
