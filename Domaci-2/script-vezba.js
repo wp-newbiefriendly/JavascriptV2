c53be258d2bc63efce8a5749dc9a77c042a3e91a
wp-newbiefriendly <locokoks@gmail.com>
2026-03-17 00:48:09 +0100

    Domaci #2 + Vezba #2

---
+++ Domaci-2/script-vezba.js
@@ -0,0 +1,60 @@
+// JavaScript vežbe – ARRAY (osnovni nivo)
+
+let brojevi = [1 ,2 ,3 ,4, 5];
+
+console.log(brojevi);
+
+console.log(brojevi[0]);
+
+console.log(brojevi[4]);
+
+let imena = ["Ana", "Marko", "Jovan"];
+
+console.log(imena[1]);
+
+imena[1] = "Nikola";
+
+console.log(imena);
+
+let ocene = [5, 4, 3];
+
+console.log(ocene[0] + ocene[1]);
+
+let gradovi = ["Beograd", "Novi Sad"];
+
+gradovi.push("Nis");
+
+console.log(gradovi);
+
+gradovi.splice(2, 1);
+
+console.log(gradovi);
+
+// Kombinovane array vežbe
+
+let brojevi2 = [10, 20, 30];
+
+console.log(brojevi2[0] + brojevi2[1] + brojevi2[2]);
+
+let cene = [100, 200, 300];
+
+cene[0] = cene[0] + 50;
+
+console.log(cene);
+
+let jezici = ["HTML", "CSS", "Java"];
+
+jezici[2] = "JavaScript";
+
+console.log(jezici);
+
+let statusi = [true, false, true];
+
+console.log(statusi[1]);
+
+let poruke = ["Zdravo", "Cao"];
+console.log(poruke[0]);
+console.log(poruke[1]);
+
+
+
---
+++ Domaci-2/script.js
@@ -0,0 +1,9 @@
+
+
+let shop = {
+    name : "Market",
+    products: ["hleb", "mleko", "jogurt"],
+    openingHours: "8am",
+    closedHours: "8pm",
+};
+console.log(shop);
